ce140376b5cb914902ec81daaa4c32ffd525d8bd
James Li <sayhello.jamesli@gmail.com>
2023-04-14 14:00:05 -0400

    check for market authority managed by PSM in get seat manager info

--- src/lib/processor/process_get_seat_manager_info.rs
+++ src/lib/processor/process_get_seat_manager_info.rs
@@ -1,4 +1,7 @@
+use std::mem::size_of;
+
 use ellipsis_client::EllipsisClient;
+use phoenix::program::MarketHeader;
 use phoenix_seat_manager::{get_seat_manager_address, seat_manager::SeatManager};
 use solana_sdk::pubkey::Pubkey;
 
@@ -9,6 +12,18 @@ pub async fn process_get_seat_manager_info(
     market_pubkey: &Pubkey,
 ) -> anyhow::Result<()> {
     let seat_manager_address = get_seat_manager_address(market_pubkey).0;
+    let market_data = client.get_account_data(market_pubkey).await?;
+    let market_header =
+        bytemuck::from_bytes::<MarketHeader>(market_data.split_at(size_of::<MarketHeader>()).0);
+    if market_header.authority != seat_manager_address {
+        println!(
+            "Authority for Market {} is not the seat manager.",
+            market_pubkey
+        );
+        println!("Market authority: {}", market_header.authority);
+        println!("Seat manager address: {}", seat_manager_address);
+        return Ok(());
+    }
     let seat_manager_info = get_seat_manager_data_with_market(client, market_pubkey).await?;
     print_seat_manager_struct(&seat_manager_info, &seat_manager_address);
     Ok(())
